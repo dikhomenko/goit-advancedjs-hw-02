7151dc4c4ef76a84e5005e11b57b7487abafcc3b
dkhomenko <dinamastercs@gmail.com>
2024-10-24 15:22:59 +0200

    add timer

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault(); // Override the form's default behavior
+
+  const delay = Number(event.target.delay.value); // Get the delay value
+  const state = event.target.state.value; // Getting the selected state (fulfilled or rejected)
+
+  // Create a promise with the selected delay
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  // Process the promise after creation
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topCenter',
+        timeout: 5000,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topCenter',
+        timeout: 5000,
+      });
+    });
+});
