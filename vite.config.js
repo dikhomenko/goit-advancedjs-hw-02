c3e17e4e68a4fdcc983e28ee5896b9af8430d1f7
dkhomenko <dinamastercs@gmail.com>
2024-10-25 17:46:12 +0200

    edit new base & build url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: 'goit-advancedjs-hw-01',
+    base: 'goit-advancedjs-hw-02',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
